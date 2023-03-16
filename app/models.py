9b6314f7ba05b772d9d03ab88fdc2201180e7a24
Zakariya Khan <zakariyavkhan@gmail.com>
2023-03-15 19:01:26 -0700

    update pytest config

--- app/models.py
+++ app/models.py
@@ -35,6 +35,7 @@ class Comment(db.Model):
         return 'Comment ID {}'.format(self.id)
     
 class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     vote_type = db.Column(db.Boolean, nullable=False) # True = upvote, False = downvote
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
