0e76453aead50a746e0150fc18804da1d226fa8c
Zakariya Khan <zakariyavkhan@gmail.com>
2023-03-19 23:37:51 -0700

    add failing test cases

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -9,3 +9,12 @@ def test_user_creation():
     new_user = User(email="test-email@gmail.com", password="abc123")
     assert new_user.email == "test-email@gmail.com"
     assert new_user.password == "abc123"
+
+def test_post_creation():
+    '''
+    GIVEN a Post model
+    WHEN a new Post is created
+    THEN check the content is defined correctly
+    '''
+    post = Post(content='post content')
+    assert post.content == 'post content'
