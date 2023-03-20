99c4b5eac072b2f496d9f12872718d90bf2ca39a
Zakariya Khan <zakariyavkhan@gmail.com>
2023-03-19 23:23:29 -0700

    add post creation code

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy import func
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +9,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.email}'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime(timezone=True), unique=False, default=func.now())
+    expired = db.Column(db.Boolean, nullable=False, default=False)
+    content = db.Column(db.String, nullable=False)
+    
+    def __repr__(self):
+        return f'Post ID {self.id}'
