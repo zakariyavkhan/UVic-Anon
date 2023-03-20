8135eec7578340eccbbbd723c44be8af10fb81af
Zakariya Khan <zakariyavkhan@gmail.com>
2023-03-19 23:42:45 -0700

    add failing test cases

---
+++ tests/integration/test_post_interaction.py
@@ -0,0 +1,10 @@
+def test_post_interaction():
+    comment = Comment(content='user 1 comment content', user_id=1, post_id=1)
+    vote = Vote(vote_type=True, user_id=1, post_id=1)
+
+    assert comment.content == 'user 1 comment content'
+    assert comment.user_id == 1
+    assert comment.post_id == 1
+    assert vote
+    assert vote.user_id == 1
+    assert vote.post_id == 1
