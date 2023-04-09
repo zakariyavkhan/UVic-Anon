654e9850d59be293f24c11869cc55d2e53ee40a7
Zakariya Khan <zakariyavkhan@gmail.com>
2023-04-08 23:54:05 -0700

    refactor variables

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -7,8 +7,8 @@ def test_user_creation():
     WHEN a new User is created
     THEN check the email and password variables are defined correctly
     '''
-    new_user = User(email="test-email@gmail.com", password="abc123")
-    assert new_user.email == "test-email@gmail.com"
+    new_user = User(username="test-email@gmail.com", password="abc123")
+    assert new_user.username == "test-email@gmail.com"
     assert new_user.password == "abc123"
 
 # TC-3
