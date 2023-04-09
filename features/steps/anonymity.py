9cc7c48c02d6a747d026b39a90545ce8cb73cc78
Zakariya Khan <zakariyavkhan@gmail.com>
2023-04-09 15:15:21 -0700

    refactor

--- features/steps/anonymity.py
+++ features/steps/anonymity.py
@@ -3,13 +3,13 @@ from behave import given, when, then
 @given(u'A user is logged in')
 def step_impl(context):
     context.browser.get('http://127.0.0.1:5000/register')
-    context.browser.find_element('name', 'username').send_keys('valid@uvic.ca')
+    context.browser.find_element('name', 'username').send_keys('test@uvic.ca')
     context.browser.find_element('name', 'password1').send_keys('password')
     context.browser.find_element('name', 'password2').send_keys('password')
     context.browser.find_element('xpath', f"//*[@id='submit']").click()
 
     context.browser.get('http://127.0.0.1:5000/login')
-    context.browser.find_element('name', 'username').send_keys('valid@uvic.ca')
+    context.browser.find_element('name', 'username').send_keys('test@uvic.ca')
     context.browser.find_element('name', 'password').send_keys('password')
     context.browser.find_element('xpath', f"//*[@id='submit']").click()
 
