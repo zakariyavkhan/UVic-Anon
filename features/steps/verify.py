8aaf3ff56203b2c582ad7c3230a0d6e68b518e55
Zakariya Khan <zakariyavkhan@gmail.com>
2023-04-09 15:27:58 -0700

    update verification feature

--- features/steps/verify.py
+++ features/steps/verify.py
@@ -22,7 +22,19 @@ def step_impl(context):
     context.browser.find_element('name', 'password1').send_keys('password')
     context.browser.find_element('name', 'password2').send_keys('password')
 
-@then(u'The username fails form validation')
+@then(u'The username not a NetlinkID')
 def step_impl(context):
     assert context.browser.current_url == 'http://127.0.0.1:5000/register'
     assert 'Not a valid NetlinkID! Please try again' in context.browser.page_source
+
+@given(u'A user tries to register with in use NetlinkID')
+def step_impl(context):
+    context.browser.get('http://127.0.0.1:5000/register')
+    context.browser.find_element('name', 'username').send_keys('valid@uvic.ca')
+    context.browser.find_element('name', 'password1').send_keys('password')
+    context.browser.find_element('name', 'password2').send_keys('password')
+
+@then(u'The username already associated with user')
+def step_impl(context):
+    assert context.browser.current_url == 'http://127.0.0.1:5000/register'
+    assert 'User already exists!' in context.browser.page_source
